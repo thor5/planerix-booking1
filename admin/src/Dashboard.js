9099baa238552958976f2c5abf7368742e8a026f
Alexander Kazantsev <4kazantsev@gmail.com>
2021-08-21 18:45:48 +0300

    feature: исправление дашборда

--- admin/src/Dashboard.js
+++ admin/src/Dashboard.js
@@ -3,10 +3,9 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { Title } from 'react-admin';
 
-const Dashboard = () => (
+export default () => (
     <Card>
         <Title title="Основные показатели БЦ" />
-        <CardContent>Статистика по кластеру на текущий момент</CardContent>
         <iframe
             src="https://app.powerbi.com/view?r=eyJrIjoiNjE3ODcwMjctODQ5MS00MjRhLWFlOWQtYjI1ZDkyNDdhMTliIiwidCI6IjJlOGEyY2FkLWM2ODgtNDkzMS1hZWI4LTk3MGJiNWYyMDgwMCIsImMiOjl9&pageName=ReportSection28b0a1fd8cdc3112c8e8"
             width="100%" height="760" align="left">
@@ -14,5 +13,3 @@ const Dashboard = () => (
         </iframe>
     </Card>
 );
-
-export default { Dashboard }
